110c9cb02227e928a378121f65226b708915095b
Christoph Safferling <christoph.safferling@bluebyte.de>
2020-10-12 17:06:43 +0200

    parse Faerun gods table done

--- pantheon.R
+++ pantheon.R
@@ -1,7 +1,27 @@
 library(tidyverse)
 library(rvest)
 
-url <- "https://www.dndbeyond.com/sources/scag/welcome-to-the-realms#TheAfterlife"
+## url of the patheon table
+#url <- "https://www.dndbeyond.com/sources/scag/welcome-to-the-realms"
+## need to download page, as SCAG is paywalled -- I forgot! 
+file <- "Welcome to the Realms - Sword Coast Adventurer's Guide - Sources - D&D Beyond.html"
+
+## read the page into R
+gods.page <- read_html(file)
+
+gods.table <- gods.page %>% 
+  html_nodes("table.faerun-deities-table") %>% 
+  html_table() %>% 
+  # flatten list into a df
+  flatten_df()
+
+
+
+
+
+
+
+
 
 
 
